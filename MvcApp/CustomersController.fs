f76cd2fa20bb9b0d263da943afbbdf1b38a35150
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2017-09-10 21:18:22 +0200

    Sample of using Saithe in MVC (#5)

---
+++ MvcApp/CustomersController.fs
@@ -0,0 +1,38 @@
+﻿namespace MvcApp.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open MvcApp.Models
+
+type CustomerModificationModel={FirstName:string ; LastName:string}
+
+[<Route("api/[controller]")>]
+type CustomersController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    [<Produces(typeof<Customer array>)>]
+    member this.Get() =
+        [|{FirstName= "value1"; LastName="Second"; Version=1; Id= CustomerId.Parse "c-y_e5TfGCE0iEKwD7Fe4XUw" } 
+          {FirstName= "value2"; LastName="Second"; Version=1; Id= CustomerId.Parse "c-4ChA7W4VAUWu24HqwBTciw" }
+        |]
+
+    [<HttpGet("{id}")>]
+    [<Produces(typeof<Customer>)>]
+    member this.Get(id:CustomerId) =
+        "value"
+
+    [<HttpPost>]
+    member this.Post([<FromBody>]value:CustomerModificationModel) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:CustomerId, [<FromBody>]value:CustomerModificationModel ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:CustomerId) =
+        ()
---
+++ MvcApp/Models.fs
@@ -0,0 +1,55 @@
+﻿module MvcApp.Models
+
+open Saithe
+open System
+open System.ComponentModel
+
+// from https://madskristensen.net/post/A-shorter-and-URL-friendly-GUID
+let toStr (x : Guid) = 
+    let enc = Convert.ToBase64String(x.ToByteArray())
+    let enc = enc.Replace("/", "_").Replace("+", "-")
+    enc.Substring(0, 22)
+
+let fromStr (encoded : string) = 
+    let encoded = encoded.Replace("_", "/").Replace("-", "+")
+    let buffer = Convert.FromBase64String(encoded + "==")
+    if buffer.Length = 16 then Some(Guid buffer)
+    else None
+
+let tryParseId (prefix : string) (str : string) = 
+    if str.StartsWith(prefix) then fromStr (str.Substring prefix.Length)
+    else None
+
+let parseId prefix str = 
+    let maybeParsed = tryParseId prefix str
+    match maybeParsed with
+    | Some g -> g
+    | None -> raise (FormatException str)
+
+[<TypeConverter(typeof<ParseTypeConverter<CustomerId>>)>]
+type CustomerId = 
+    { Value : Guid }
+    static member Default : CustomerId = { Value=Guid.Empty }
+    static member Parse(str : string) : CustomerId = { Value = parseId "c-" str }
+    override this.ToString() = sprintf "c-%s" (toStr this.Value)
+
+[<TypeConverter(typeof<ParseTypeConverter<ProductId>>)>]
+type ProductId = 
+    { Value : Guid }
+    static member Default : ProductId = { Value=Guid.Empty }
+    static member Parse(str : string) : ProductId = { Value = parseId "p-" str }
+    override this.ToString() = sprintf "p-%s" (toStr this.Value)
+
+[<TypeConverter(typeof<ParseTypeConverter<OrderId>>)>]
+type OrderId = 
+    { Value : Guid }
+    static member Default : OrderId = { Value=Guid.Empty }
+    static member Parse(str : string) : OrderId = { Value = parseId "o-" str }
+    override this.ToString() = sprintf "o-%s" (toStr this.Value)
+
+
+type Customer = {Id:CustomerId; FirstName:string ; LastName:string; Version:int}
+
+type Product = {Id:ProductId; Cost:decimal; Name: string; Version: int}
+
+type Order = {Id:OrderId; Customer:Customer; OrderDate:DateTime; Products: Product list; Version: int}
